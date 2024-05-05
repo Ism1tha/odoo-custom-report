31bcd7e7522a7b349d2fcd12a3a3e2269c01bc64
Ism1tha <xgalan@hotmail.com>
2024-05-05 16:28:52 +0200

    Practica acabada

--- models/sale_order.py
+++ models/sale_order.py
@@ -6,21 +6,6 @@ class SaleOrder(models.Model):
     customer_email = fields.Char(string='Adreça de correu preferencial del client')
     num_articles = fields.Integer(string='Nombre d\'articles', compute='_compute_num_articles')
 
-    commercial_name = fields.Char(string='Commercial Name', related='user_id.name')
-    state_count = fields.Integer(string='State Count', compute='_compute_state_count')
-    total_amount = fields.Float(string='Total Amount', compute='_compute_total_amount')
-
     def _compute_num_articles(self):
         for order in self:
             order.num_articles = len(order.order_line)
-
-    @api.depends('commercial_name', 'state')
-    def _compute_state_count(self):
-        for order in self:
-            order.state_count = self.search_count([('user_id', '=', order.user_id.id), ('state', '=', order.state)])
-
-    @api.depends('commercial_name', 'state')
-    def _compute_total_amount(self):
-        for order in self:
-            orders = self.search([('user_id', '=', order.user_id.id), ('state', '=', order.state)])
-            order.total_amount = sum(orders.mapped('amount_total'))
